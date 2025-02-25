079a2ce1ba6fb14e04bb55dfeb700b58ba504467
fajrul <fajrulsantoso10@gmail.com>
2025-02-25 11:25:33 +0700

    Pesan commit Anda

--- database/migrations/2025_02_25_035349_create_m_barang_table.php
+++ database/migrations/2025_02_25_035349_create_m_barang_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\DBAL\TimestampType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('m_barang', function (Blueprint $table) {
-            //
+            $table->string('barang_id',20);
+            $table->string('kategori_id',20);
+            $table->string('barang_kode',10)->unique();
+            $table->string('barang_nama',100);
+            $table->string('harga_beli',11);
+            $table->string('harga_jual',11);
+
+
         });
     }
 
