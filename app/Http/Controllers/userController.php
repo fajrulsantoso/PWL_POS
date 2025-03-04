3866c3ad1fe3e1cb44c2cc8f24bc97135631d0ab
fajrul <fajrulsantoso10@gmail.com>
2025-03-04 09:24:43 +0700

    JOBSHEET_4_PRAKTIKUM 2.2

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -11,15 +11,7 @@ class userController extends Controller
 {
     public function index()
     {
-        // $user = UserModel::find0r(20, ['username','nama'], function () {
-        //     abort(404);
-        // });
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-
-        // coba akses model UserModel
-        // $user = UserModel::firstwhere('level_id', 1); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+           return view('user', ['data' => $user]);
     }
 }
