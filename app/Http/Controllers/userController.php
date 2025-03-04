9346dbfe44fc2d6de12f016d7bca9dfd2267abfe
fajrul <fajrulsantoso10@gmail.com>
2025-03-04 08:26:58 +0700

    JOBSHEET_4_PRAKTIKUM 1

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use view;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,10 +13,13 @@ class userController extends Controller
     {
         // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => '2',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
 
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+        UserModel::created($data); // update data user
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,4 +10,7 @@ class UserModel extends Model
     use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+     
+     protected $fillable =['level_id','username','nama','password'];
+
 }
